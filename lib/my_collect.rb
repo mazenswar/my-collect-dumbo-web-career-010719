34eecda05a66b4d0e2cedb200f833b0efcfcb9d8
mazenswar <mazenswar@protonmail.com>
2018-12-02 18:19:51 -0500

    my collect

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect arr
+  i = 0
+  new_collection = []
+  if block_given?
+    while i < arr.length
+      new_collection << yield(arr[i])
+      i += 1
+    end
+  else
+    arr
+  end
+  new_collection
+end
